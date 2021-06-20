ea903fbbc573446ba2f0ab268bf077b8950cf3db
kiancn <kian.tema@gmail.com>
2021-06-20 19:10:58 +0200

    Updaed Songs and Level Goals

---
+++ Assets/CheatWinLevel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatWinLevel : MonoBehaviour
+{
+    [SerializeField] private KeyCode cheatCode = KeyCode.Comma;
+
+    public void FinishLevel()
+    {
+        
+    }
+}
